2ab1711483a74bce62c66cd30bc43ec0bd2e4a70
Niklas Carstensen <kobidogao@yahoo.com>
2024-04-28 04:18:45 +0200

    Improve Widget

--- NotesAndroid/Manager.cs
+++ NotesAndroid/Manager.cs
@@ -1,6 +1,8 @@
 ﻿using Android.Content;
 using Notes.Interface;
 using Configuration;
+using System.Text;
+using System.Diagnostics;
 
 namespace NotesAndroid
 {
@@ -15,10 +17,26 @@ namespace NotesAndroid
             widgetView.SetTextViewText(Resource.Id.widgetUpdateText,
                 string.Format("Last update: {0:H:mm:ss}", DateTime.Now));
 
-            widgetView.SetTextViewText(Resource.Id.widgetNoteView,
-                    Config.Data.Notes.Select(x => x.Text).Combine("\n"));
+            //Debug.WriteLine("bulding shit");
+            lock (Config.Data)
+                widgetView.SetTextViewText(Resource.Id.widgetNoteView,
+                    GetWidgetText(Config.Data.Notes).ToString());
+            //Debug.WriteLine("doone!");
 
             widgetView.SetTextColor(Resource.Id.widgetNoteView, Android.Graphics.Color.Black);
+        }
+
+        public static StringBuilder GetWidgetText(List<Note> Notes, StringBuilder? re = null, int depth = 0)
+        {
+            re ??= new();
+            foreach (Note note in Notes)
+            {
+                re.AppendLine(new string(Enumerable.Repeat(' ', depth * 2).ToArray()) + note.Text);
+                if (note.Expanded)
+                    GetWidgetText(note.SubNotes, re, depth + 1);
+            }
+            //Debug.WriteLine($"text: {re}");
+            return re;
         }
     }
 }
