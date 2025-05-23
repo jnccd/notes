97f8461d9745380a06d8f88c29854a5d1284440d
jnccd <kobidogao@outlook.com>
2025-05-23 19:28:14 +0200

    Update shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,7 @@
 # nix-shell --command "bash update_and_start.sh"
 # export NIXPKGS_ALLOW_INSECURE=1 && nix-shell --impure --command "bash update_and_start.sh"
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 with pkgs;
 mkShell {
-  packages = [
-    dotnet-sdk
-  ];
+  packages = [ icu dotnet-sdk dotnet-ef ];
 }
