26eca7d59c1ff9afc210dc333c3361b990d5a601
jnccd <kobidogao@outlook.com>
2025-07-29 17:33:47 +0200

    Fix warn

--- NotesInterface/Extensions.cs
+++ NotesInterface/Extensions.cs
@@ -25,7 +25,7 @@ namespace Notes.Interface
         public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
         {
             var tcs = new TaskCompletionSource<bool>();
-            using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
+            using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s!).TrySetResult(true), tcs))
             {
                 if (task != await Task.WhenAny(task, tcs.Task))
                 {
