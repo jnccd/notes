ab94924cd5dd990dd2f5bc111ec6b8e9dd67b34c
Dobiko <kobidogao@yahoo.com>
2026-05-09 21:44:42 +0200

    Add migration

---
+++ NotesServer/Migrations/20260509194433_SplitUserIdNameHandle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class SplitUserIdNameHandle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "UserHandle");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserDisplayName",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserDisplayName",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "UserHandle",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+    }
+}
