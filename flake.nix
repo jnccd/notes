390ed2d75f590a379a9d8f058a5de8cfb7dff48e
jnccd <kobidogao@outlook.com>
2026-06-09 19:22:16 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,24 +2,37 @@
   description = "Nix Shell Wrapper";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    numtide-utils.url = "github:numtide/flake-utils";
-    jnccd-utils.url = "github:jnccd/nix-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    numtide-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    jnccd-utils = {
+      url = "github:jnccd/nix-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    inputs.numtide-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    inputs.numtide-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
         devShells = rec {
           service = import ./shell.nix { inherit pkgs; };
           gui = inputs.jnccd-utils.lib.mkUnfrozenDotnetShell {
             inherit system nixpkgs;
             dotnetVersion = "9.0";
-            androidSdkVersions = [ "34" "35" ];
+            androidSdkVersions = [
+              "34"
+              "35"
+            ];
           };
 
           default = gui;
         };
-      });
+      }
+    );
 }
