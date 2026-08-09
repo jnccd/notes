57f7a737b094bcbfb28a345e97bb8e28e331b4b0
Dobiko <kobidogao@yahoo.com>
2026-08-09 19:14:32 +0200

    Disable data migrator

--- NotesServer/DataMigrator.cs
+++ NotesServer/DataMigrator.cs
@@ -8,21 +8,21 @@ public static class DataMigrator
 {
     public static void Migrate()
     {
-        var dbContext = new NotesDbContext(new DbContextOptions<NotesDbContext>());
+        // var dbContext = new NotesDbContext(new DbContextOptions<NotesDbContext>());
 
-        foreach (var user in dbContext.Users)
-        {
-            user.NotesPayload?.GetAllNotes().ForEach(x =>
-            {
-                x.Note.Data.Text = x.Note.Text ?? "";
-                x.Note.Data.Done = x.Note.Done ?? false;
-                x.Note.Data.Expanded = x.Note.Expanded ?? false;
-                x.Note.Data.Hidden = x.Note.Hidden ?? false;
-                x.Note.Data.Prio = x.Note.Prio ?? NotePriority.Medium;
-            });
-            user.NotesPayload?.SaveTime = DateTime.Now;
-        }
+        // foreach (var user in dbContext.Users)
+        // {
+        //     user.NotesPayload?.GetAllNotes().ForEach(x =>
+        //     {
+        //         x.Note.Data.Text = x.Note.Text ?? "";
+        //         x.Note.Data.Done = x.Note.Done ?? false;
+        //         x.Note.Data.Expanded = x.Note.Expanded ?? false;
+        //         x.Note.Data.Hidden = x.Note.Hidden ?? false;
+        //         x.Note.Data.Prio = x.Note.Prio ?? NotePriority.Medium;
+        //     });
+        //     user.NotesPayload?.SaveTime = DateTime.Now;
+        // }
 
-        dbContext.SaveChanges();
+        // dbContext.SaveChanges();
     }
 }
