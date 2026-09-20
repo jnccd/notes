b1660deb380474d4dcce68c643ea679a2954508d
jnccd <kobidogao@outlook.com>
2026-09-20 15:59:42 +0200

    Improve auth

--- NotesServer/Services/Auth/AuthReqResult.cs
+++ NotesServer/Services/Auth/AuthReqResult.cs
@@ -8,7 +8,7 @@ public class AuthReqResult : IResult
     {
         httpContext.Response.StatusCode = 401;
 #pragma warning disable ASP0019 // There should not be a duplicate key, so let it crash in that case
-        httpContext.Response.Headers.Add("Www-Authenticate", "Basic realm=\"private\"");
+        httpContext.Response.Headers.Add("Www-Authenticate", "Bearer realm=\"private\"");
         httpContext.Response.Headers.Add("Connection", "close");
 #pragma warning restore ASP0019
         return Task.FromResult(httpContext.Response);
